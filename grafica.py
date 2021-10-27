5f7884f2f9e97edad3f360cdcb27f1618e0e50a2
Fabio Biscaro <fabio.biscaro@gmail.com>
2021-10-27 17:21:18 +0200

    grafica aggiornata

--- grafica.py
+++ grafica.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 
-layout = [[sg.Text("Saluti da PySimpleGUI")], [sg.InputText("Inserisci qualcosa",key="pippo")], [sg.Button("OK")]] 
+layout = [[sg.Text("Saluti da PySimpleGUI")], [sg.InputText("Inserisci qualcosa",key="-pippo-")], [sg.Button("OK")]] 
 
 # Create the window
 window = sg.Window("Prima prova", layout)
@@ -14,9 +14,9 @@ window = sg.Window("Prima prova", layout)
 # Create an event loop
 while True:
     event, values = window.read()
-    text_input = values["pippo"]    
+    text_input = values["-pippo-"]    
     sg.popup('Hai scritto:', text_input)
-    window["pippo"].update("mamma")
+    window["-pippo-"].update("mamma")
     # End program if user closes window or
     # presses the OK button
     # if event == "OK" or event == sg.WIN_CLOSED:
