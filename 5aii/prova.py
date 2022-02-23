58b758beb2d4af92c5f831ab6488c8c8b2b8582c
fabio <fabio.biscaro@gmail.com>
2022-02-23 15:44:56 +0100

    Calcolatrice ad oggetti

--- 5aii/prova.py
+++ 5aii/prova.py
@@ -1,11 +1,22 @@
+# https://pysimplegui.readthedocs.io/en/latest/
 import PySimpleGUI as sg
-sg.theme('DarkAmber')
-layout = [[sg.Text("Ciao")],[sg.InputText()],[sg.Button("Ok")]]
 
-window = sg.Window("Prova", layout)
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
 
-window.read()
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
 
+window.close()
 
 print("你好 mondo")
 print("你好 mondo")
